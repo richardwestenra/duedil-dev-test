9fdbd427dbf157b269005d0772c0a369d44dab43
Richard Westenra <rjwestenra@gmail.com>
2015-12-28 16:58:25 +0000

    Add issues and get a rough design together

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,38 @@
-(function(){
+/* eslint-disable no-console */
+/* globals Mustache */
 
-})();
+(function(window, $){
+
+  var $issues = $('#issues'),
+    issueTemplate = $('#issue-template').html();
+
+
+  function makeList (issues) {
+    // Create HTML from template
+    $issues.html( Mustache.render(
+      issueTemplate,
+      { issues }
+    ))
+    // Bind data to HTML
+    .find('li').each(function (i) {
+      $(this).data( issues[i] );
+    });
+    // .end()
+    // .on('click','li',function (){
+    //   console.log($(this).data());
+    // });
+  }
+
+
+
+  $(function(){
+    $.getJSON('data/issues.json', makeList);
+  });
+
+
+  // Export to Window
+  // window.app = window.app || {};
+  // window.app.main = {
+  // };
+
+})(window, jQuery);
