0c091b3b7d0a2c58a7a18aa22df2a6d131c9fae0
Richard Westenra <rjwestenra@gmail.com>
2016-01-02 18:14:33 +0000

    Add and style filter select inputs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,26 @@
 /* eslint-disable no-console */
 /* globals Mustache */
 
-(function(window, $){
+(function (window, $) {
 
-  var $issues = $('#issues'),
+  const categories = ['project','category','assignee'];
+
+  const $issues = $('#issues'),
+    $clear = $('#filter-clear'),
     issueTemplate = $('#issue-template').html();
 
+  var selectizes = [],
+    // flag to avoid infinite causal loop:
+    activeFilter = false;
 
-  function makeList (issues) {
+
+  // Add the jQuery unique function to the array prototype to allow chaining:
+  Array.prototype.$unique = function () {
+    return $.unique(this);
+  }
+
+
+  function makeIssueList(issues) {
     // Create HTML from template
     $issues.html( Mustache.render(
       issueTemplate,
@@ -19,8 +32,35 @@
     });
   }
 
-  function filterList (type, value) {
-    $('#filters').html(`${type}: ${value}`)
+
+  function preventLoop(callback) {
+    activeFilter = true;
+    callback();
+    activeFilter = false;
+  }
+
+
+  function filterIssueList(type, value) {
+    $('#filters').html(value);
+
+    preventLoop(() => {
+      selectizes.forEach(s => {
+        if (s.name === type && value) {
+          s.api.setValue(value);
+        } else {
+          s.api.clear();
+        }
+      });
+    });
+
+    if (!value) {
+      $clear.fadeOut(200);
+      $issues.find('li').removeClass('hidden');
+      return;
+    }
+
+    $clear.fadeIn(200);
+
     $issues.find('li').each(function () {
       var datum = $(this).data();
       var hidden = datum[type] !== value;
@@ -29,26 +69,71 @@
   }
 
 
+  function getFilterData(data) {
+    function formatData (key) {
+      return data.map(d => d[key])
+        .filter(d => !!d)
+        .$unique()
+        .sort();
+    }
+
+    return function (key) {
+      var list = formatData(key);
+      return makeFilterNav(key,list);
+    };
+  }
+
+
+  function makeFilterNav(key, data) {
+
+    var selectItems = data.map(d => {
+      return { value: d };
+    });
+
+    var selectize = $(`#filter-${key}`).selectize({
+      plugins: ['remove_button'],
+      allowEmptyOption: true,
+      valueField: 'value',
+      labelField: 'value',
+      searchField: 'value',
+      options: selectItems,
+      onChange: function (d){
+        if (!activeFilter) {
+          filterIssueList(key, d);
+        }
+      }
+    })[0].selectize;
+
+    return {
+      name: key,
+      api: selectize
+    };
+  }
+
+
+
+  // Initialise:
 
   $(function(){
-    $.getJSON('data/issues.json', makeList);
 
-    $issues.on('click', '.issue_filter', function (e){
-      var data = $(this).parents('li').data();
-      var type = $(this).data('type');
-      var value = $(this).text();
-      console.log(type, value);
-      filterList(type, value);
+    $.getJSON('data/issues.json', (data) => {
+      selectizes = categories.map( getFilterData(data) );
+      makeIssueList(data);
+    });
 
-
+    $clear.on('click', function (e){
+      filterIssueList();
       e.preventDefault();
     });
+
+    $issues.on('click', '.issue_filter', function (e){
+      var type = $(this).data('type');
+      var value = $(this).text();
+      filterIssueList(type, value);
+      e.preventDefault();
+    });
+
   });
 
-
-  // Export to Window
-  // window.app = window.app || {};
-  // window.app.main = {
-  // };
 
 })(window, jQuery);
